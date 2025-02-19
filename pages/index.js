cd90ae8589b2c7a33150c9b34efd38acaa2b48d9
bmossy <bmossy7@gmail.com>
2025-02-18 20:26:50 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>MongoDB Sales Intelligence Tool</h1>
+            <p>This tool helps you compare MongoDB with competitors and generate sales pitches.</p>
+        </div>
+    );
+}
